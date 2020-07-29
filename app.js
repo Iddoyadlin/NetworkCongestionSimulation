5021e7ee9f68ecc6ce87ef65b9b55eeda03b054c
iddoyadlin <iddoyadlin@gmail.com>
2020-07-29 22:01:05 +0300

    handle cost function flipping

--- app.js
+++ app.js
@@ -100,6 +100,8 @@ const dragLine = svg.append('svg:path')
 let path = svg.append('svg:g').selectAll('path');
 let circle = svg.append('svg:g').selectAll('g');
 
+
+
 // mouse event vars
 let selectedNode = null;
 let selectedLink = null;
@@ -119,7 +121,7 @@ function resetMouseVars() {
 // update force layout (called automatically each iteration)
 function tick() {
   // draw directed edges with proper padding from node centers
-  path.attr('d', (d) => {
+  path.attr('d', (d, i) => {
     const deltaX = d.target.x - d.source.x;
     const deltaY = d.target.y - d.source.y;
     const dist = Math.sqrt(deltaX * deltaX + deltaY * deltaY);
@@ -132,10 +134,21 @@ function tick() {
     const targetX = d.target.x - (targetPadding * normX);
     const targetY = d.target.y - (targetPadding * normY);
 
+    if (d.cost!=null){
+      if (sourceX < targetX){
+        svg.selectAll('textPath').attr('href', '#linkId_' + i).text(d.cost)
+      }
+      else{
+        svg.selectAll('textPath').attr('href', '#linkId_' + i).text(flipString(d.cost))
+      }
+    }
+
+
     return `M${sourceX},${sourceY}L${targetX},${targetY}`;
   });
 
   circle.attr('transform', (d) => `translate(${d.x},${d.y})`);
+  
 }
 
 // update graph (called when needed)
@@ -273,7 +286,7 @@ function restart() {
       // select new link
       selectedLink = link;
       selectedNode = null;
-      selectedPlayer = null
+      
       restart();
     });
 
@@ -685,3 +698,89 @@ d3.select(window)
 
 restart();
 selectPlayer(0)
+
+
+
+function flipString(aString)
+{
+  var last = aString.length - 1;
+  //Thanks to Brook Monroe for the
+  //suggestion to use Array.join
+  var result = new Array(aString.length)
+  for (var i = last; i >= 0; --i)
+  {
+    var c = aString.charAt(i)
+    var r = flipTable[c]
+    result[last - i] = r != undefined ? r : c
+  }
+  return result.join('')
+}
+
+var flipTable = {
+'\u0021' : '\u00A1',
+'\u0022' : '\u201E',
+'\u0026' : '\u214B',
+'\u0027' : '\u002C',
+'\u0028' : '\u0029',
+'\u002E' : '\u02D9',
+'\u0033' : '\u0190',
+'\u0034' : '\u152D',
+'\u0036' : '\u0039',
+'\u0037' : '\u2C62',
+'\u003B' : '\u061B',
+'\u003C' : '\u003E',
+'\u003F' : '\u00BF',
+'\u0041' : '\u2200',
+'\u0042' : '\u10412',
+'\u0043' : '\u2183',
+'\u0044' : '\u25D6',
+'\u0045' : '\u018E',
+'\u0046' : '\u2132',
+'\u0047' : '\u2141',
+'\u004A' : '\u017F',
+'\u004B' : '\u22CA',
+'\u004C' : '\u2142',
+'\u004D' : '\u0057',
+'\u004E' : '\u1D0E',
+'\u0050' : '\u0500',
+'\u0051' : '\u038C',
+'\u0052' : '\u1D1A',
+'\u0054' : '\u22A5',
+'\u0055' : '\u2229',
+'\u0056' : '\u1D27',
+'\u0059' : '\u2144',
+'\u005B' : '\u005D',
+'\u005F' : '\u203E',
+'\u0061' : '\u0250',
+'\u0062' : '\u0071',
+'\u0063' : '\u0254',
+'\u0064' : '\u0070',
+'\u0065' : '\u01DD',
+'\u0066' : '\u025F',
+'\u0067' : '\u0183',
+'\u0068' : '\u0265',
+'\u0069' : '\u0131',
+'\u006A' : '\u027E',
+'\u006B' : '\u029E',
+'\u006C' : '\u0283',
+'\u006D' : '\u026F',
+'\u006E' : '\u0075',
+'\u0072' : '\u0279',
+'\u0074' : '\u0287',
+'\u0076' : '\u028C',
+'\u0077' : '\u028D',
+'\u0079' : '\u028E',
+'\u007B' : '\u007D',
+'\u203F' : '\u2040',
+'\u2045' : '\u2046',
+'\u2234' : '\u2235',
+'2': '\u218A',
+'^': '\u2304',
+'x': '\u0058',
+'1': '\u21c2'
+}
+
+for (i in flipTable)
+{
+  flipTable[flipTable[i]] = i
+}
